458de6575c4ca391fec6293a1b99e14f80dfd71c
francisadhi <francis.adhi@gmail.com>
2022-03-16 09:32:32 +0700

    restore image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ import WelcomeSection from '@components/WelcomeSection';
 import HelloSection from '@components/HelloSection';
 import WeddingSection from '@components/WeddingSection';
 import LocationSection from '@components/LocationSection';
-import StorySection from '@components/StorySection';
-import PhotoSection from '@components/PhotoSection/Loadable';
-import WishesSection from '@components/WishesSection';
 import ConfirmationSection from '@components/ConfirmationSection';
 import FooterSection from '@components/FooterSection';
 import CovidSection from '@components/Covid19';
@@ -41,9 +38,6 @@ function Home({ location }) {
         <WeddingSection isInvitation={isInvitation} />
         {isInvitation && <CovidSection />}
         {isInvitation && <LocationSection />}
-        {/* <StorySection /> */}
-        {/* <PhotoSection /> */}
-        {/* <WishesSection /> */}
         <ConfirmationSection guestName={firstName} isInvitation={isInvitation} codeLink={finalTicketLink} />
         <FooterSection isInvitation={isInvitation} />
       </Fragment>
