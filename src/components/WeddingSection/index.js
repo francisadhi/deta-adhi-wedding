a550ce1bf88d79b572dac42a224b1eeead909eaa
francisadhi <francis.adhi@gmail.com>
2022-03-22 14:51:40 +0700

    animate

--- src/components/WeddingSection/index.js
+++ src/components/WeddingSection/index.js
@@ -4,6 +4,7 @@ import { bool } from 'prop-types';
 import WeddingInfoBox from './WeddingInfoBox';
 import ButtonLive from './ButtonLive';
 import { styWrapper } from './styles';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function WeddingSection({ isInvitation }) {
   const renderGuestInfo = () => {
@@ -22,44 +23,52 @@ function WeddingSection({ isInvitation }) {
       <div id="fh5co-event" css={styWrapper}>
         <div className="overlay section--bg-fixed" />
         <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-              <span className="bismillah">Mohon Doa Restu</span>
-              <h2 className="main-font main-font__wedding">Sakramen dan Resepsi Pernikahan</h2>
-              <span className="sub-title sub-title__wedding">Akan diselenggarakan pada:</span>
+          <AnimationOnScroll animateIn="animate__fadeInUp">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <span className="bismillah">Mohon Doa Restu</span>
+                <h2 className="main-font main-font__wedding">Sakramen dan Resepsi Pernikahan</h2>
+                <span className="sub-title sub-title__wedding">Akan diselenggarakan pada:</span>
+              </div>
             </div>
-          </div>
+          </AnimationOnScroll>
           <div className="row animated animatedFadeInUp fadeInUp">
             {!isInvitation && renderGuestInfo()}
             {isInvitation && (
               <div className="col-md-10 col-md-offset-1">
-                <WeddingInfoBox
-                  title="Sakramen Pernikahan"
-                  time="Akan dilaksanakan pada pukul 10.00 WIB "
-                  date="Sabtu, 02 Juli 2022"
-                  description="Gereja Santo Yusuf Pekerja <br/>Mertoyudan, Magelang"
-                />
-                <WeddingInfoBox
-                  title="Resepsi Pernikahan"
-                  time="Akan dilaksanakan pada pukul 14.00 WIB "
-                  date="Sabtu, 02 Juli 2022"
-                  description="Balkondes Duta Menoreh <br/>Desa Tanjungsari, Borobudur"
-                />
+                <AnimationOnScroll animateIn="animate__bounceInLeft">
+                  <WeddingInfoBox
+                    title="Sakramen Pernikahan"
+                    time="Akan dilaksanakan pada pukul 10.00 WIB "
+                    date="Sabtu, 02 Juli 2022"
+                    description="Gereja Santo Yusuf Pekerja <br/>Mertoyudan, Magelang"
+                  />
+                </AnimationOnScroll>
+                <AnimationOnScroll animateIn="animate__bounceInRight">
+                  <WeddingInfoBox
+                    title="Resepsi Pernikahan"
+                    time="Akan dilaksanakan pada pukul 14.00 WIB "
+                    date="Sabtu, 02 Juli 2022"
+                    description="Balkondes Duta Menoreh <br/>Desa Tanjungsari, Borobudur"
+                  />
+                </AnimationOnScroll>
               </div>
             )}
           </div>
           <br />
           <br />
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-              <p className="sub-title sub-title__wedding">Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
-berkenan hadir untuk memberikan doa restu kepada kami
-Atas kehadiran dan doa restunya kami ucapkan terima kasih</p>
+          <AnimationOnScroll animateIn="animate__fadeInUp">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <p className="sub-title sub-title__wedding">Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
+                  berkenan hadir untuk memberikan doa restu kepada kami
+                  Atas kehadiran dan doa restunya kami ucapkan terima kasih</p>
+              </div>
             </div>
-          </div>
+          </AnimationOnScroll>
         </div>
       </div>
-    </Fragment>
+    </Fragment >
   );
 }
 
